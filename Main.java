1dc15fa3dec1b761660c3bde7c32b3c246d7fd87
Franco3010 <35970825+Franco3010@users.noreply.github.com>
2021-01-05 22:14:15 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import com.google.gson.Gson;
+
+import model.GetListVideo;
+import model.GetRequestFriend;
+import model.Login;
+import response.ObjectReponse;
+
+public class Main {
+	
+	
+	public static void main(String[] args){
+	//new SignUp().signUp("0935161909","012345679");//"0366426401", "012345679"
+	//new Login().login();
+	new GetRequestFriend().getRequestFriend();	
+	//new GetListVideo().getListVideo();
+	}
+
+}
