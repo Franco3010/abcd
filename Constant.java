5d89ae107c199b6042d5e5dcb1ae9a5553b954d5
Franco3010 <35970825+Franco3010@users.noreply.github.com>
2021-01-05 22:00:55 +0700

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constant {
+	public static final String FILE_GetRequestFriends = "GetRequestFriends.txt";
+	
+	public static final String LOGIN = "https://it4895-nhom5.herokuapp.com/it4788/auth/login";
+	public static final String GET_REQUEST_FRIENDS = "https://it4895-nhom5.herokuapp.com/it4788/friend/get_requested_friends";
+	public static final String GET_LIST_VIDEO = "https://it4895-nhom5.herokuapp.com/it4788/post/get_list_videos";
+	public static final String LOGOUT = "https://it4895-nhom5.herokuapp.com/it4788/auth/logout";
+	public static final String GET_USER_FRIENDS = "http://it4895.herokuapp.com/it4895/user/friends";
+}
